4471ef853c2c8ff20a103aacddc9962ef77c2691
Lauzaki <84672368+lauzaki@users.noreply.github.com>
2021-10-18 12:56:28 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+
+//test
+export const nftaddress = "0x08eB094c70A9B5A7a868572D3C5A16f5Eb97CcEC"
+export const nftmarketaddress = "0x0F128A3007DcfF537cA5b8569Ea8170dB3413883"
+//local
+//export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+//export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString();
+const infuraId = fs.readFileSync(".infuraid").toString();
+
+console.log(privateKey)
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+
+    mumbai: {
+      // Infura
+      url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    },
+    matic: {
+      // Infura
+      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
